378d2a7ff05b7a037400de01c4f2fe4e9c9af22d
Serhey Popovych <serhe.popovych@gmail.com>
2017-11-06 19:19:54 +0200

    libxt_CLASSIFY: Implement revision 1 support
    Permit modification of existing value of skb->priority field
    to complete/change final qdisc class of the packet.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- extensions/libxt_CLASSIFY.c
+++ extensions/libxt_CLASSIFY.c
@@ -18,6 +18,22 @@ enum {
 	/* revision 0 */
 	F_REV0		= F_SET_CLASS,
 	F_REV0_ALL	= F_COMMON | F_REV0,
+
+	/* revision 1 */
+	O_AND_CLASS	= 1,
+	O_OR_CLASS	= 2,
+	O_XOR_CLASS	= 3,
+	O_SET_XCLASS	= 4,
+
+	F_AND_CLASS	= (1 << O_AND_CLASS),
+	F_OR_CLASS	= (1 << O_OR_CLASS),
+	F_XOR_CLASS	= (1 << O_XOR_CLASS),
+	F_SET_XCLASS	= (1 << O_SET_XCLASS),
+	F_ANY_CLASS	= F_SET_CLASS | F_AND_CLASS | F_OR_CLASS |
+			  F_XOR_CLASS | F_SET_XCLASS,
+
+	F_REV1		= F_AND_CLASS | F_OR_CLASS | F_XOR_CLASS | F_SET_XCLASS,
+	F_REV1_ALL	= F_COMMON | F_REV1,
 };
 
 static const struct xt_option_entry classify_opts[] = {
@@ -25,6 +41,31 @@ static const struct xt_option_entry classify_opts[] = {
 		.name	= "set-class",
 		.id	= O_SET_CLASS,
 		.type	= XTTYPE_STRING,
+		.excl	= F_ANY_CLASS,
+	},
+	[O_AND_CLASS] = {
+		.name	= "and-class",
+		.id	= O_AND_CLASS,
+		.type	= XTTYPE_UINT32,
+		.excl	= F_ANY_CLASS,
+	},
+	[O_OR_CLASS] = {
+		.name	= "or-class",
+		.id	= O_OR_CLASS,
+		.type	= XTTYPE_UINT32,
+		.excl	= F_ANY_CLASS,
+	},
+	[O_XOR_CLASS] = {
+		.name	= "xor-class",
+		.id	= O_XOR_CLASS,
+		.type	= XTTYPE_UINT32,
+		.excl	= F_ANY_CLASS,
+	},
+	[O_SET_XCLASS] = {
+		.name	= "set-xclass",
+		.id	= O_SET_XCLASS,
+		.type	= XTTYPE_MARKMASK32,
+		.excl	= F_ANY_CLASS,
 	},
 	XTOPT_TABLEEND,
 };
@@ -42,12 +83,12 @@ static int classify_parse_priority(const char *s, unsigned int *p)
 	return 0;
 }
 
-static void classify_print_priority(const char *pfx, unsigned int p)
+static void classify_print_priority(unsigned int p)
 {
 	printf(" %x:%x", TC_H_MAJ(p) >> 16, TC_H_MIN(p));
 }
 
-static void CLASSIFY_help(void)
+static void CLASSIFY_help_v0(void)
 {
 	printf(
 "CLASSIFY target options:\n"
@@ -55,15 +96,58 @@ static void CLASSIFY_help(void)
 	);
 }
 
+static void CLASSIFY_help(void)
+{
+	printf(
+"CLASSIFY target options:\n"
+"  --set-class MAJOR:MINOR    Set skb->priority value\n"
+"or\n"
+"  --set-xclass value[/mask]  Clear bits in mask and XOR value into CLASS\n"
+"  --set-class value[/mask]   Clear bits in mask and OR value into CLASS\n"
+"  --and-class bits           Binary AND the CLASS with bits\n"
+"  --or-class bits            Binary OR the CLASS with bits\n"
+"  --xor-class bits           Binary XOR the CLASS with bits\n"
+	);
+}
+
 static void
 CLASSIFY_show(const char *pfx, const struct xt_entry_target *target)
 {
 	const struct xt_classify_target_info *info = (const void *) target->data;
+	const struct xt_classify_tginfo *info1 = (const void *) target->data;
+	const unsigned int revision = target->u.user.revision;
+	unsigned int mode, priority;
 
 	if (!pfx)
 		pfx = "";
 
-	classify_print_priority(pfx, info->priority);
+	/* revision >= 1 */
+
+	if (revision < 1 || info1->mask == ~0U) {
+		printf(" %s%s", pfx, classify_opts[O_SET_CLASS].name);
+		classify_print_priority(info->priority);
+		if (revision < 1)
+			return;
+	} else {
+		if (info1->priority == 0) {
+			mode = O_AND_CLASS;
+			priority = ~info1->mask;
+		} else {
+			if (info1->priority == info1->mask)
+				mode = O_OR_CLASS;
+			else if (info1->mask == 0)
+				mode = O_XOR_CLASS;
+			else
+				mode = O_SET_XCLASS;
+			priority = info1->priority;
+		}
+
+		printf(" %s%s", pfx, classify_opts[mode].name);
+
+		printf(" 0x%x", priority);
+		if (mode == O_SET_XCLASS)
+			printf("/0x%x", info1->mask);
+	}
 }
 
 static void
@@ -83,6 +167,7 @@ CLASSIFY_save(const void *ip, const struct xt_entry_target *target)
 static void CLASSIFY_parse(struct xt_option_call *cb)
 {
 	struct xt_classify_target_info *info = cb->data;
+	struct xt_classify_tginfo *info1 = cb->data;
 	const unsigned int revision = (*cb->target)->u.user.revision;
 	unsigned int id;
 
@@ -92,25 +177,62 @@ static void CLASSIFY_parse(struct xt_option_call *cb)
 	switch (id) {
 	case O_SET_CLASS:
 		/* MAJ:MIN */
-		if (!classify_parse_priority(cb->arg, &info->priority))
-			break;
+		if (!classify_parse_priority(cb->arg, &info->priority)) {
+			/* revision >= 1 */
+			if (revision > 0)
+				info1->mask = -1;
+			return;
+		}
 
-		xtables_error(PARAMETER_PROBLEM,
-			     "Bad class value \"%s\"", cb->arg);
-	default:
-		xtables_error(PARAMETER_PROBLEM,
-			      "libxt_CLASSIFY.%u does not support --%s",
-			      revision,
-			      classify_opts[id].name);
+		/* revision >= 1 */
+		if (revision < 1)
+			xtables_error(PARAMETER_PROBLEM,
+				     "Bad class value \"%s\"", cb->arg);
+
+		xtables_parse_mark_mask(cb, &info1->priority, &info1->mask);
+		info1->mask |= info1->priority;
+		return;
 	}
+
+	/* revision >= 1 */
+	if (revision < 1)
+		goto no_supp;
+
+	switch (id) {
+	case O_AND_CLASS:
+		info1->priority = 0;
+		info1->mask = ~cb->val.u32;
+		break;
+	case O_OR_CLASS:
+		info1->priority = info1->mask = cb->val.u32;
+		break;
+	case O_XOR_CLASS:
+		info1->priority = cb->val.u32;
+		info1->mask = 0;
+		break;
+	case O_SET_XCLASS:
+		info1->priority = cb->val.mark;
+		info1->mask = cb->val.mask;
+		break;
+	default:
+		goto no_supp;
+	}
+
+	return;
+
+no_supp:
+	xtables_error(PARAMETER_PROBLEM,
+		      "libxt_CLASSIFY.%u does not support --%s",
+		      revision,
+		      classify_opts[id].name);
 }
 
 static void CLASSIFY_check(struct xt_fcheck_call *cb)
 {
-	if (cb->xflags == 0) {
+	if (!(cb->xflags & F_ANY_CLASS)) {
 		xtables_error(PARAMETER_PROBLEM,
-			      "CLASSIFY target: Parameter --set-class"
-			      "is required");
+			      "CLASSIFY: One of the --set-xclass, "
+			      "--{and,or,xor,set}-class options is required");
 	}
 }
 
@@ -151,15 +273,31 @@ static struct xtables_target classify_tg_reg[] = {
 		.family		= NFPROTO_UNSPEC,
 		.name		= "CLASSIFY",
 		.version	= XTABLES_VERSION,
+		.revision	= 0,
 		.size		= XT_ALIGN(sizeof(struct xt_classify_target_info)),
 		.userspacesize	= XT_ALIGN(sizeof(struct xt_classify_target_info)),
+		.help		= CLASSIFY_help_v0,
+		.print		= CLASSIFY_print,
+		.save		= CLASSIFY_save,
+		.xlate		= CLASSIFY_xlate,
+		.x6_parse	= CLASSIFY_parse,
+		.x6_fcheck	= CLASSIFY_check,
+		.x6_options	= classify_opts,
+	},
+	{
+		.family		= NFPROTO_UNSPEC,
+		.name		= "CLASSIFY",
+		.version	= XTABLES_VERSION,
+		.revision	= 1,
+		.size		= XT_ALIGN(sizeof(struct xt_classify_tginfo)),
+		.userspacesize	= XT_ALIGN(sizeof(struct xt_classify_tginfo)),
 		.help		= CLASSIFY_help,
 		.print		= CLASSIFY_print,
 		.save		= CLASSIFY_save,
+		.xlate		= CLASSIFY_xlate,
 		.x6_parse	= CLASSIFY_parse,
 		.x6_fcheck	= CLASSIFY_check,
-		.x6_options	= CLASSIFY_opts,
-		.xlate		= CLASSIFY_xlate,
+		.x6_options	= classify_opts,
 	},
 	{
 		.family		= NFPROTO_ARP,
@@ -169,10 +307,10 @@ static struct xtables_target classify_tg_reg[] = {
 		.userspacesize	= XT_ALIGN(sizeof(struct xt_classify_target_info)),
 		.help		= CLASSIFY_help,
 		.print		= arpCLASSIFY_print,
+		.xlate		= CLASSIFY_xlate,
 		.x6_parse	= CLASSIFY_parse,
 		.x6_fcheck	= CLASSIFY_check,
-		.x6_options	= CLASSIFY_opts,
-		.xlate		= CLASSIFY_xlate,
+		.x6_options	= classify_opts,
 	},
 };
 
--- include/linux/netfilter/xt_CLASSIFY.h
+++ include/linux/netfilter/xt_CLASSIFY.h
@@ -3,8 +3,15 @@
 
 #include <linux/types.h>
 
+/* revision 0 */
 struct xt_classify_target_info {
 	__u32 priority;
+};
+
+/* revision 1 */
+struct xt_classify_tginfo {
+	__u32 priority;
+	__u32 mask;
 };
 
 #endif /*_XT_CLASSIFY_H */
