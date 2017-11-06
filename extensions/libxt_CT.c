8f07526b6d0f3f46da59bacaf5dcb30d1d953f7b
Serhey Popovych <serhe.popovych@gmail.com>
2017-11-06 19:19:21 +0200

    libxt_CT: Cleanup and optimize CT target
    Check options mutual exclusion when possible.
    
    Merge common code in help, print, save and parse
    routines.
    
    Make code more readable.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- extensions/libxt_CT.c
+++ extensions/libxt_CT.c
@@ -8,75 +8,91 @@
 #include <linux/netfilter/nf_conntrack_common.h>
 #include <linux/netfilter/xt_CT.h>
 
-static void ct_help(void)
-{
-	printf(
-"CT target options:\n"
-" --notrack			Don't track connection\n"
-" --helper name			Use conntrack helper 'name' for connection\n"
-" --ctevents event[,event...]	Generate specified conntrack events for connection\n"
-" --expevents event[,event...]	Generate specified expectation events for connection\n"
-" --zone {ID|mark}		Assign/Lookup connection in zone ID/packet nfmark\n"
-" --zone-orig {ID|mark}		Same as 'zone' option, but only applies to ORIGINAL direction\n"
-" --zone-reply {ID|mark} 	Same as 'zone' option, but only applies to REPLY direction\n"
-	);
-}
-
-static void ct_help_v1(void)
-{
-	printf(
-"CT target options:\n"
-" --notrack			Don't track connection\n"
-" --helper name			Use conntrack helper 'name' for connection\n"
-" --timeout name 		Use timeout policy 'name' for connection\n"
-" --ctevents event[,event...]	Generate specified conntrack events for connection\n"
-" --expevents event[,event...]	Generate specified expectation events for connection\n"
-" --zone {ID|mark}		Assign/Lookup connection in zone ID/packet nfmark\n"
-" --zone-orig {ID|mark}		Same as 'zone' option, but only applies to ORIGINAL direction\n"
-" --zone-reply {ID|mark} 	Same as 'zone' option, but only applies to REPLY direction\n"
-	);
-}
-
 enum {
-	O_NOTRACK = 0,
-	O_HELPER,
-	O_TIMEOUT,
-	O_CTEVENTS,
-	O_EXPEVENTS,
-	O_ZONE,
-	O_ZONE_ORIG,
-	O_ZONE_REPLY,
+	/* common */
+	O_NOTRACK	= 0,
+	O_ZONE		= 1,
+	O_CTEVENTS	= 2,
+	O_EXPEVENTS	= 3,
+	O_HELPER	= 4,
+
+	F_NOTRACK	= (1 << O_NOTRACK),
+	F_ZONE		= (1 << O_ZONE),
+	F_CTEVENTS	= (1 << O_CTEVENTS),
+	F_EXPEVENTS	= (1 << O_EXPEVENTS),
+	F_HELPER	= (1 << O_HELPER),
+
+	F_COMMON	= F_NOTRACK | F_ZONE | F_CTEVENTS |
+			  F_EXPEVENTS | F_HELPER,
+
+	/* revision 1, 2 */
+	O_TIMEOUT	= 5,
+	O_ZONE_ORIG	= 6,
+	O_ZONE_REPLY	= 7,
+
+	F_TIMEOUT	= (1 << O_TIMEOUT),
+	F_ZONE_ORIG	= (1 << O_ZONE_ORIG),
+	F_ZONE_REPLY	= (1 << O_ZONE_REPLY),
+
+	F_ZONE_ALL	= F_ZONE|F_ZONE_ORIG|F_ZONE_REPLY,
+
+	F_REV1		= F_TIMEOUT|F_ZONE_ORIG|F_ZONE_REPLY,
+	F_REV1_ALL	= F_COMMON | F_REV1,
 };
 
-#define s struct xt_ct_target_info
 static const struct xt_option_entry ct_opts[] = {
-	{.name = "notrack", .id = O_NOTRACK, .type = XTTYPE_NONE},
-	{.name = "helper", .id = O_HELPER, .type = XTTYPE_STRING,
-	 .flags = XTOPT_PUT, XTOPT_POINTER(s, helper)},
-	{.name = "ctevents", .id = O_CTEVENTS, .type = XTTYPE_STRING},
-	{.name = "expevents", .id = O_EXPEVENTS, .type = XTTYPE_STRING},
-	{.name = "zone-orig", .id = O_ZONE_ORIG, .type = XTTYPE_STRING},
-	{.name = "zone-reply", .id = O_ZONE_REPLY, .type = XTTYPE_STRING},
-	{.name = "zone", .id = O_ZONE, .type = XTTYPE_STRING},
+	[O_NOTRACK] = {
+		.name	= "notrack",
+		.id	= O_NOTRACK,
+		.type	= XTTYPE_NONE,
+		.excl	= F_ZONE_ALL|F_CTEVENTS|F_EXPEVENTS|F_HELPER|F_TIMEOUT,
+	},
+	[O_ZONE] = {
+		.name	= "zone",
+		.id	= O_ZONE,
+		.type	= XTTYPE_UINT16,
+		.excl	= F_NOTRACK,
+	},
+	[O_CTEVENTS] = {
+		.name	= "ctevents",
+		.id	= O_CTEVENTS,
+		.type	= XTTYPE_STRING,
+		.excl	= F_NOTRACK,
+	},
+	[O_EXPEVENTS] = {
+		.name	= "expevents",
+		.id	= O_EXPEVENTS,
+		.type	= XTTYPE_STRING,
+		.excl	= F_NOTRACK,
+	},
+	[O_HELPER] = {
+		.name	= "helper",
+		.id	= O_HELPER,
+		.type	= XTTYPE_STRING,
+		.flags	= XTOPT_PUT,
+		XTOPT_POINTER(struct xt_ct_target_info, helper),
+		.excl	= F_NOTRACK,
+	},
+	[O_TIMEOUT] = {
+		.name	= "timeout",
+		.id	= O_TIMEOUT,
+		.type	= XTTYPE_STRING,
+		.excl	= F_NOTRACK,
+	},
+	[O_ZONE_ORIG] = {
+		.name	= "zone-orig",
+		.id	= O_ZONE_ORIG,
+		.type	= XTTYPE_STRING,
+		.excl	= F_NOTRACK,
+	},
+	[O_ZONE_REPLY] = {
+		.name	= "zone-reply",
+		.id	= O_ZONE_REPLY,
+		.type	= XTTYPE_STRING,
+		.excl	= F_NOTRACK,
+	},
 	XTOPT_TABLEEND,
 };
-#undef s
-
-#define s struct xt_ct_target_info_v1
-static const struct xt_option_entry ct_opts_v1[] = {
-	{.name = "notrack", .id = O_NOTRACK, .type = XTTYPE_NONE},
-	{.name = "helper", .id = O_HELPER, .type = XTTYPE_STRING,
-	 .flags = XTOPT_PUT, XTOPT_POINTER(s, helper)},
-	{.name = "timeout", .id = O_TIMEOUT, .type = XTTYPE_STRING,
-	 .flags = XTOPT_PUT, XTOPT_POINTER(s, timeout)},
-	{.name = "ctevents", .id = O_CTEVENTS, .type = XTTYPE_STRING},
-	{.name = "expevents", .id = O_EXPEVENTS, .type = XTTYPE_STRING},
-	{.name = "zone-orig", .id = O_ZONE_ORIG, .type = XTTYPE_STRING},
-	{.name = "zone-reply", .id = O_ZONE_REPLY, .type = XTTYPE_STRING},
-	{.name = "zone", .id = O_ZONE, .type = XTTYPE_STRING},
-	XTOPT_TABLEEND,
-};
-#undef s
 
 struct event_tbl {
 	const char	*name;
@@ -125,14 +141,19 @@ static void ct_parse_zone_id(const char *opt, unsigned int opt_id,
 
 static void ct_print_zone_id(const char *pfx, uint16_t zone_id, uint16_t flags)
 {
-	printf(" %s", pfx);
+	const char *opt;
 
-	if ((flags & (XT_CT_ZONE_DIR_ORIG |
-		      XT_CT_ZONE_DIR_REPL)) == XT_CT_ZONE_DIR_ORIG)
-		printf("-orig");
-	if ((flags & (XT_CT_ZONE_DIR_ORIG |
-		      XT_CT_ZONE_DIR_REPL)) == XT_CT_ZONE_DIR_REPL)
-		printf("-reply");
+	switch (flags & (XT_CT_ZONE_DIR_ORIG|XT_CT_ZONE_DIR_REPL)) {
+	case XT_CT_ZONE_DIR_ORIG:
+		opt = "-orig";
+		break;
+	case XT_CT_ZONE_DIR_REPL:
+		opt = "-reply";
+		break;
+	}
+
+	printf(" %szone%s", pfx, opt);
+
 	if (flags & XT_CT_ZONE_MARK)
 		printf(" mark");
 	else
@@ -161,13 +182,17 @@ static uint32_t ct_parse_events(const struct event_tbl *tbl, unsigned int size,
 	return mask;
 }
 
-static void ct_print_events(const char *pfx, const struct event_tbl *tbl,
+static void ct_print_events(const char *pfx, const char *opt,
+			    const struct event_tbl *tbl,
 			    unsigned int size, uint32_t mask)
 {
 	const char *sep = "";
 	unsigned int i;
 
-	printf(" %s ", pfx);
+	if (!pfx)
+		pfx = "";
+
+	printf(" %s%s ", pfx, opt);
 	for (i = 0; i < size; i++) {
 		if (mask & (1 << tbl[i].event)) {
 			printf("%s%s", sep, tbl[i].name);
@@ -176,44 +201,101 @@ static void ct_print_events(const char *pfx, const struct event_tbl *tbl,
 	}
 }
 
-static void ct_parse(struct xt_option_call *cb)
+static void CT_help_v0(void)
 {
-	struct xt_ct_target_info *info = cb->data;
-
-	xtables_option_parse(cb);
-	switch (cb->entry->id) {
-	case O_NOTRACK:
-		info->flags |= XT_CT_NOTRACK;
-		break;
-	case O_ZONE_ORIG:
-	case O_ZONE_REPLY:
-	case O_ZONE:
-		ct_parse_zone_id(cb->arg, cb->entry->id, &info->zone,
-				 &info->flags);
-		break;
-	case O_CTEVENTS:
-		info->ct_events = ct_parse_events(ct_event_tbl, ARRAY_SIZE(ct_event_tbl), cb->arg);
-		break;
-	case O_EXPEVENTS:
-		info->exp_events = ct_parse_events(exp_event_tbl, ARRAY_SIZE(exp_event_tbl), cb->arg);
-		break;
-	}
+	printf(
+"CT target options:\n"
+" --notrack                     Don't track connection\n"
+" --zone ID                     Assign/Lookup connection in zone ID\n"
+" --ctevents event[,event...]   Generate specified conntrack events for connection\n"
+" --expevents event[,event...]  Generate specified expectation events for connection\n"
+" --helper name                 Use conntrack helper 'name' for connection\n"
+" --zone {ID|mark}              Assign/Lookup connection in zone ID/packet nfmark\n"
+" --zone-orig {ID|mark}         Same as 'zone' option, but only applies to ORIGINAL direction\n"
+" --zone-reply {ID|mark}        Same as 'zone' option, but only applies to REPLY direction\n"
+	);
 }
 
-static void ct_parse_v1(struct xt_option_call *cb)
+static void CT_help(void)
 {
-	struct xt_ct_target_info_v1 *info = cb->data;
+	CT_help_v0();
+	printf(
+" --timeout name                Use timeout policy 'name' for connection\n"
+	);
+}
+
+static void CT_show(const char *pfx, const struct xt_entry_target *target)
+{
+	const struct xt_ct_target_info *info = (const void *) target->data;
+	const struct xt_ct_target_info_v1 *info1 = (const void *) target->data;
+	const unsigned int revision = target->u.user.revision;
+
+	if (info->flags & XT_CT_NOTRACK)
+		printf(" %snotrack", pfx);
+	if (info->helper[0])
+		printf(" %shelper %s", pfx, info->helper);
+	if (info->ct_events)
+		ct_print_events(pfx, "ctevents", ct_event_tbl,
+				ARRAY_SIZE(ct_event_tbl), info->ct_events);
+	if (info->exp_events)
+		ct_print_events(pfx, "expevents", exp_event_tbl,
+				ARRAY_SIZE(exp_event_tbl), info->exp_events);
+	if ((info->flags & XT_CT_ZONE_MARK) || info->zone)
+		ct_print_zone_id(pfx, info->zone, info->flags);
+
+	/* revision >= 1 */
+	if (revision < 1)
+		return;
+
+	if (info1->timeout[0])
+		printf(" %stimeout %s", pfx, info1->timeout);
+}
+
+static void CT_print(const void *ip, const struct xt_entry_target *target,
+		     int numeric)
+{
+	const struct xt_ct_target_info *info = (const void *) target->data;
+
+	if (info->flags & XT_CT_NOTRACK_ALIAS) {
+		fputs(" NOTRACK", stdout);
+		return;
+	}
+
+	fputs(" CT", stdout);
+	CT_show("", target);
+}
+
+static void CT_save(const void *ip, const struct xt_entry_target *target)
+{
+	const struct xt_ct_target_info *info = (const void *) target->data;
+
+	if (info->flags & XT_CT_NOTRACK_ALIAS)
+		return;
+
+	CT_show("--", target);
+}
+
+static void CT_parse(struct xt_option_call *cb)
+{
+	struct xt_ct_target_info *info = cb->data;
+	struct xt_ct_target_info_v1 *info1 = cb->data;
+	const unsigned int revision = (*cb->target)->u.user.revision;
+	unsigned int id;
 
 	xtables_option_parse(cb);
-	switch (cb->entry->id) {
+	id = cb->entry->id;
+
+	switch (id) {
 	case O_NOTRACK:
 		info->flags |= XT_CT_NOTRACK;
 		break;
 	case O_ZONE_ORIG:
 	case O_ZONE_REPLY:
+		/* revision >= 1 */
+		if (revision < 1)
+			goto no_supp;
 	case O_ZONE:
-		ct_parse_zone_id(cb->arg, cb->entry->id, &info->zone,
-				 &info->flags);
+		ct_parse_zone_id(cb->arg, id, &info->zone, &info->flags);
 		break;
 	case O_CTEVENTS:
 		info->ct_events = ct_parse_events(ct_event_tbl,
@@ -225,127 +307,44 @@ static void ct_parse_v1(struct xt_option_call *cb)
 						   ARRAY_SIZE(exp_event_tbl),
 						   cb->arg);
 		break;
+	case O_HELPER:
+		break;
+	case O_TIMEOUT:
+		/* revision >= 1 */
+		if (revision < 1)
+			goto no_supp;
+		snprintf(info1->timeout, sizeof(info1->timeout), "%s", cb->arg);
+		break;
+	default:
+		goto no_supp;
 	}
-}
 
-static void ct_print(const void *ip, const struct xt_entry_target *target, int numeric)
-{
-	const struct xt_ct_target_info *info =
-		(const struct xt_ct_target_info *)target->data;
+	return;
 
-	printf(" CT");
-	if (info->flags & XT_CT_NOTRACK)
-		printf(" notrack");
-	if (info->helper[0])
-		printf(" helper %s", info->helper);
-	if (info->ct_events)
-		ct_print_events("ctevents", ct_event_tbl,
-				ARRAY_SIZE(ct_event_tbl), info->ct_events);
-	if (info->exp_events)
-		ct_print_events("expevents", exp_event_tbl,
-				ARRAY_SIZE(exp_event_tbl), info->exp_events);
-	if (info->flags & XT_CT_ZONE_MARK || info->zone)
-		ct_print_zone_id("zone", info->zone, info->flags);
-}
-
-static void
-ct_print_v1(const void *ip, const struct xt_entry_target *target, int numeric)
-{
-	const struct xt_ct_target_info_v1 *info =
-		(const struct xt_ct_target_info_v1 *)target->data;
-
-	if (info->flags & XT_CT_NOTRACK_ALIAS) {
-		printf (" NOTRACK");
-		return;
-	}
-	printf(" CT");
-	if (info->flags & XT_CT_NOTRACK)
-		printf(" notrack");
-	if (info->helper[0])
-		printf(" helper %s", info->helper);
-	if (info->timeout[0])
-		printf(" timeout %s", info->timeout);
-	if (info->ct_events)
-		ct_print_events("ctevents", ct_event_tbl,
-				ARRAY_SIZE(ct_event_tbl), info->ct_events);
-	if (info->exp_events)
-		ct_print_events("expevents", exp_event_tbl,
-				ARRAY_SIZE(exp_event_tbl), info->exp_events);
-	if (info->flags & XT_CT_ZONE_MARK || info->zone)
-		ct_print_zone_id("zone", info->zone, info->flags);
-}
-
-static void ct_save(const void *ip, const struct xt_entry_target *target)
-{
-	const struct xt_ct_target_info *info =
-		(const struct xt_ct_target_info *)target->data;
-
-	if (info->flags & XT_CT_NOTRACK_ALIAS)
-		return;
-	if (info->flags & XT_CT_NOTRACK)
-		printf(" --notrack");
-	if (info->helper[0])
-		printf(" --helper %s", info->helper);
-	if (info->ct_events)
-		ct_print_events("--ctevents", ct_event_tbl,
-				ARRAY_SIZE(ct_event_tbl), info->ct_events);
-	if (info->exp_events)
-		ct_print_events("--expevents", exp_event_tbl,
-				ARRAY_SIZE(exp_event_tbl), info->exp_events);
-	if (info->flags & XT_CT_ZONE_MARK || info->zone)
-		ct_print_zone_id("--zone", info->zone, info->flags);
-}
-
-static void ct_save_v1(const void *ip, const struct xt_entry_target *target)
-{
-	const struct xt_ct_target_info_v1 *info =
-		(const struct xt_ct_target_info_v1 *)target->data;
-
-	if (info->flags & XT_CT_NOTRACK_ALIAS)
-		return;
-	if (info->flags & XT_CT_NOTRACK)
-		printf(" --notrack");
-	if (info->helper[0])
-		printf(" --helper %s", info->helper);
-	if (info->timeout[0])
-		printf(" --timeout %s", info->timeout);
-	if (info->ct_events)
-		ct_print_events("--ctevents", ct_event_tbl,
-				ARRAY_SIZE(ct_event_tbl), info->ct_events);
-	if (info->exp_events)
-		ct_print_events("--expevents", exp_event_tbl,
-				ARRAY_SIZE(exp_event_tbl), info->exp_events);
-	if (info->flags & XT_CT_ZONE_MARK || info->zone)
-		ct_print_zone_id("--zone", info->zone, info->flags);
+no_supp:
+	xtables_error(PARAMETER_PROBLEM,
+		      "libxt_CT.%u does not support --%s",
+		      revision,
+		      ct_opts[id].name);
 }
 
 static const char *
-ct_print_name_alias(const struct xt_entry_target *target)
+CT_print_name_alias(const struct xt_entry_target *target)
 {
 	struct xt_ct_target_info *info = (void *)target->data;
 
-	return info->flags & XT_CT_NOTRACK_ALIAS ? "NOTRACK" : "CT";
+	return (info->flags & XT_CT_NOTRACK_ALIAS) ? "NOTRACK" : "CT";
 }
 
-static void notrack_ct0_tg_init(struct xt_entry_target *target)
+static void NOTRACK_ct_tg_init(struct xt_entry_target *target)
 {
-	struct xt_ct_target_info *info = (void *)target->data;
+	struct xt_ct_target_info_v1 *info = (void *) target->data;
+	const unsigned int revision = target->u.user.revision;
 
-	info->flags = XT_CT_NOTRACK;
-}
-
-static void notrack_ct1_tg_init(struct xt_entry_target *target)
-{
-	struct xt_ct_target_info_v1 *info = (void *)target->data;
-
-	info->flags = XT_CT_NOTRACK;
-}
-
-static void notrack_ct2_tg_init(struct xt_entry_target *target)
-{
-	struct xt_ct_target_info_v1 *info = (void *)target->data;
-
-	info->flags = XT_CT_NOTRACK | XT_CT_NOTRACK_ALIAS;
+	if (revision < 2)
+		info->flags = XT_CT_NOTRACK;
+	else
+		info->flags = XT_CT_NOTRACK | XT_CT_NOTRACK_ALIAS;
 }
 
 static struct xtables_target ct_target_reg[] = {
@@ -355,10 +354,10 @@ static struct xtables_target ct_target_reg[] = {
 		.version	= XTABLES_VERSION,
 		.size		= XT_ALIGN(sizeof(struct xt_ct_target_info)),
 		.userspacesize	= offsetof(struct xt_ct_target_info, ct),
-		.help		= ct_help,
-		.print		= ct_print,
-		.save		= ct_save,
-		.x6_parse	= ct_parse,
+		.help		= CT_help_v0,
+		.print		= CT_print,
+		.save		= CT_save,
+		.x6_parse	= CT_parse,
 		.x6_options	= ct_opts,
 	},
 	{
@@ -368,11 +367,11 @@ static struct xtables_target ct_target_reg[] = {
 		.version	= XTABLES_VERSION,
 		.size		= XT_ALIGN(sizeof(struct xt_ct_target_info_v1)),
 		.userspacesize	= offsetof(struct xt_ct_target_info_v1, ct),
-		.help		= ct_help_v1,
-		.print		= ct_print_v1,
-		.save		= ct_save_v1,
-		.x6_parse	= ct_parse_v1,
-		.x6_options	= ct_opts_v1,
+		.help		= CT_help,
+		.print		= CT_print,
+		.save		= CT_save,
+		.x6_parse	= CT_parse,
+		.x6_options	= ct_opts,
 	},
 	{
 		.family		= NFPROTO_UNSPEC,
@@ -381,49 +380,49 @@ static struct xtables_target ct_target_reg[] = {
 		.version	= XTABLES_VERSION,
 		.size		= XT_ALIGN(sizeof(struct xt_ct_target_info_v1)),
 		.userspacesize	= offsetof(struct xt_ct_target_info_v1, ct),
-		.help		= ct_help_v1,
-		.print		= ct_print_v1,
-		.save		= ct_save_v1,
-		.alias		= ct_print_name_alias,
-		.x6_parse	= ct_parse_v1,
-		.x6_options	= ct_opts_v1,
+		.help		= CT_help,
+		.print		= CT_print,
+		.save		= CT_save,
+		.alias		= CT_print_name_alias,
+		.x6_parse	= CT_parse,
+		.x6_options	= ct_opts,
 	},
 	{
-		.family        = NFPROTO_UNSPEC,
-		.name          = "NOTRACK",
-		.real_name     = "CT",
-		.revision      = 0,
-		.version       = XTABLES_VERSION,
-		.size          = XT_ALIGN(sizeof(struct xt_ct_target_info)),
-		.userspacesize = offsetof(struct xt_ct_target_info, ct),
-		.init          = notrack_ct0_tg_init,
+		.family		= NFPROTO_UNSPEC,
+		.name		= "NOTRACK",
+		.real_name	= "CT",
+		.revision	= 0,
+		.version	= XTABLES_VERSION,
+		.size		= XT_ALIGN(sizeof(struct xt_ct_target_info)),
+		.userspacesize	= offsetof(struct xt_ct_target_info, ct),
+		.init		= NOTRACK_ct_tg_init,
 	},
 	{
-		.family        = NFPROTO_UNSPEC,
-		.name          = "NOTRACK",
-		.real_name     = "CT",
-		.revision      = 1,
-		.version       = XTABLES_VERSION,
-		.size          = XT_ALIGN(sizeof(struct xt_ct_target_info_v1)),
-		.userspacesize = offsetof(struct xt_ct_target_info_v1, ct),
-		.init          = notrack_ct1_tg_init,
+		.family		= NFPROTO_UNSPEC,
+		.name		= "NOTRACK",
+		.real_name	= "CT",
+		.revision	= 1,
+		.version	= XTABLES_VERSION,
+		.size		= XT_ALIGN(sizeof(struct xt_ct_target_info_v1)),
+		.userspacesize	= offsetof(struct xt_ct_target_info_v1, ct),
+		.init		= NOTRACK_ct_tg_init,
 	},
 	{
-		.family        = NFPROTO_UNSPEC,
-		.name          = "NOTRACK",
-		.real_name     = "CT",
-		.revision      = 2,
-		.ext_flags     = XTABLES_EXT_ALIAS,
-		.version       = XTABLES_VERSION,
-		.size          = XT_ALIGN(sizeof(struct xt_ct_target_info_v1)),
-		.userspacesize = offsetof(struct xt_ct_target_info_v1, ct),
-		.init          = notrack_ct2_tg_init,
+		.family		= NFPROTO_UNSPEC,
+		.name		= "NOTRACK",
+		.real_name	= "CT",
+		.revision	= 2,
+		.ext_flags	= XTABLES_EXT_ALIAS,
+		.version	= XTABLES_VERSION,
+		.size		= XT_ALIGN(sizeof(struct xt_ct_target_info_v1)),
+		.userspacesize	= offsetof(struct xt_ct_target_info_v1, ct),
+		.init		= NOTRACK_ct_tg_init,
 	},
 	{
-		.family        = NFPROTO_UNSPEC,
-		.name          = "NOTRACK",
-		.revision      = 0,
-		.version       = XTABLES_VERSION,
+		.family		= NFPROTO_UNSPEC,
+		.name		= "NOTRACK",
+		.revision	= 0,
+		.version	= XTABLES_VERSION,
 	},
 };
 
