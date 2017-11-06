fcfcbc9c1412a6cc2199531a17615b7e4412274c
Serhey Popovych <serhe.popovych@gmail.com>
2017-11-06 23:49:20 +0200

    libxt_socket: add revision 3 support
    Introduce support for matching socket state, ownership, whole match
    result inversion and invert result of option transparent among with
    support for match inversion in other options (except nowildcard).
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- include/linux/netfilter/xt_socket.h
+++ include/linux/netfilter/xt_socket.h
@@ -7,12 +7,25 @@ enum {
 	XT_SOCKET_TRANSPARENT	= 1 << 0,
 	XT_SOCKET_NOWILDCARD	= 1 << 1,
 	XT_SOCKET_RESTORESKMARK = 1 << 2,
+	XT_SOCKET_INVERT	= 1 << 3,
+	XT_SOCKET_STATE		= 1 << 4,
+	XT_SOCKET_USER		= 1 << 5,
+	XT_SOCKET_GROUP		= 1 << 6,
 #ifdef __KERNEL__
 	XT_SOCKET_FLAGS_V1	= (XT_SOCKET_TRANSPARENT),
 	XT_SOCKET_FLAGS_V2	= (XT_SOCKET_FLAGS_V1|
 				   XT_SOCKET_NOWILDCARD),
 	XT_SOCKET_FLAGS_V3	= (XT_SOCKET_FLAGS_V2|
 				   XT_SOCKET_RESTORESKMARK),
+	XT_SOCKET_FLAGS_V4	= (XT_SOCKET_FLAGS_V3|
+				   XT_SOCKET_INVERT|
+				   XT_SOCKET_STATE|
+				   XT_SOCKET_USER|
+				   XT_SOCKET_GROUP),
+	XT_SOCKET_INVFLAGS_V3	= (XT_SOCKET_TRANSPARENT|
+				   XT_SOCKET_STATE|
+				   XT_SOCKET_USER|
+				   XT_SOCKET_GROUP),
 #endif
 };
 
@@ -26,6 +39,14 @@ struct xt_socket_mtinfo2 {
 
 struct xt_socket_mtinfo3 {
 	__u8 flags;
+};
+
+struct xt_socket_mtinfo4 {
+	__u8 flags;
+	__u8 invflags;
+	__u32 state;
+	__u32 uid_min, uid_max;
+	__u32 gid_min, gid_max;
 };
 
 #endif /* _XT_SOCKET_H */
