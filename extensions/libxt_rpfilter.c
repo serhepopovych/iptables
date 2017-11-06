c1a4d7eb7b28e47382a7cb750a3829ffd94126ac
Serhey Popovych <serhe.popovych@gmail.com>
2017-11-06 19:51:20 +0200

    libxt_rpfilter: add revision 1 support
    Implement userspace part for revision 1 of ipt_rpfilter and
    ip6t_rpfilter which introduces support of prefixlen and devgroup
    match options.
    
    Make examples in man page more readable.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- extensions/libxt_rpfilter.c
+++ extensions/libxt_rpfilter.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <xtables.h>
 #include <linux/netfilter/xt_rpfilter.h>
 
@@ -7,6 +9,8 @@ enum {
 	O_VMARK,
 	O_ACCEPT_LOCAL,
 	O_INVERT,
+	O_PREFIXLEN,
+	O_DEVGROUP,
 };
 
 static const struct xt_option_entry rpfilter_opts[] = {
@@ -30,8 +34,24 @@ static const struct xt_option_entry rpfilter_opts[] = {
 		.id	= O_INVERT,
 		.type	= XTTYPE_NONE,
 	},
+	[O_PREFIXLEN] = {
+		.name	= "prefixlen",
+		.id	= O_PREFIXLEN,
+		.type	= XTTYPE_PLEN,
+		.flags	= XTOPT_INVERT,
+	},
+	[O_DEVGROUP] = {
+		.name	= "devgroup",
+		.id	= O_DEVGROUP,
+		.type	= XTTYPE_STRING,
+		.flags	= XTOPT_INVERT,
+	},
 	XTOPT_TABLEEND,
 };
+
+static const char f_devgroups[] = "/etc/iproute2/group";
+/* array of devgroups from f_devgroups[] */
+static struct xtables_lmap *devgroups;
 
 static void rpfilter_help(void)
 {
@@ -41,14 +61,19 @@ static void rpfilter_help(void)
 "    --validmark              use skb nfmark when performing route lookup\n"
 "    --accept-local           do not reject packets with a local source address\n"
 "    --invert                 match packets that failed the reverse path test\n"
+"[!] --prefixlen <length>     match if reverse path route prefix length is\n"
+"                             shorter than or equal to length\n"
+"[!] --devgroup value[/mask]  match if reverse path route outgoing interface is\n"
+"                             in device group\n"
 	);
 }
 
 static void rpfilter_parse(struct xt_option_call *cb)
 {
-	struct xt_rpfilter_info *rpfinfo = cb->data;
+	struct xt_rpfilter_mtinfo1 *info = cb->data;
 	const unsigned int revision = (*cb->match)->u.user.revision;
 	unsigned int id, flags;
+	unsigned int group, mask;
 
 	xtables_option_parse(cb);
 	id = cb->entry->id;
@@ -66,19 +91,57 @@ static void rpfilter_parse(struct xt_option_call *cb)
 	case O_INVERT:
 		flags = XT_RPFILTER_INVERT;
 		break;
+	case O_PREFIXLEN:
+		/* revision >= 1 */
+		if (revision < 1)
+			goto no_supp;
+		info->prefixlen = cb->val.hlen;
+		flags = XT_RPFILTER_PREFIXLEN;
+		if (cb->invert)
+			flags |= XT_RPFILTER_PREFIXLEN_INVERT;
+		break;
+	case O_DEVGROUP:
+		/* revision >= 1 */
+		if (revision < 1)
+			goto no_supp;
+		xtables_parse_val_mask(cb, &group, &mask, devgroups);
+		info->group = group;
+		info->group_mask = mask;
+		flags = XT_RPFILTER_GROUP;
+		if (cb->invert)
+			flags |= XT_RPFILTER_GROUP_INVERT;
+		break;
 	default:
-		xtables_error(PARAMETER_PROBLEM,
-			      "libxt_rpfilter.%u does not support --%s",
-			      revision,
-			      rpfilter_opts[id].name);
+		goto no_supp;
 	}
 
-	rpfinfo->flags |= flags;
+	info->flags |= flags;
+	return;
+
+no_supp:
+	xtables_error(PARAMETER_PROBLEM,
+		      "libxt_rpfilter.%u does not support %s--%s",
+		      revision,
+		      cb->invert ? "! " : "",
+		      rpfilter_opts[id].name);
 }
 
 static void
-rpfilter_show(const char *pfx, unsigned int flags)
+rpfilter_show(const char *pfx, const struct xt_entry_match *match, bool numeric)
 {
+	const struct xt_rpfilter_mtinfo1 *info = (const void *) match->data;
+	const unsigned int revision = match->u.user.revision;
+	unsigned int flags = info->flags;
+	const char *inv;
+
+	if (!pfx)
+		pfx = "";
+
+	if (*pfx == '\0')
+		printf(" rpfilter");
+
+	/* revision >= 0 */
+
 	if (flags & XT_RPFILTER_LOOSE)
 		printf(" %s%s", pfx, rpfilter_opts[O_LOOSE].name);
 	if (flags & XT_RPFILTER_VALID_MARK)
@@ -87,30 +150,45 @@ rpfilter_show(const char *pfx, unsigned int flags)
 		printf(" %s%s", pfx, rpfilter_opts[O_ACCEPT_LOCAL].name);
 	if (flags & XT_RPFILTER_INVERT)
 		printf(" %s%s", pfx, rpfilter_opts[O_INVERT].name);
+
+	/* revision >= 1 */
+
+	if (revision < 1)
+		return;
+
+	if (flags & XT_RPFILTER_PREFIXLEN) {
+		inv = (flags & XT_RPFILTER_PREFIXLEN_INVERT) ? "! " : "";
+		printf(" %s%s%s %u",
+			inv, pfx,
+			rpfilter_opts[O_PREFIXLEN].name,
+			info->prefixlen);
+	}
+
+	if (flags & XT_RPFILTER_GROUP) {
+		inv = (flags & XT_RPFILTER_GROUP_INVERT) ? "! " : "";
+		printf(" %s%s%s",
+			inv, pfx,
+			rpfilter_opts[O_DEVGROUP].name);
+		xtables_print_val_mask(info->group, info->group_mask,
+				       numeric ? NULL : devgroups);
+	}
 }
 
 static void
-rpfilter_print(const void *ip, const struct xt_entry_match *match,
-	       int numeric)
+rpfilter_print(const void *ip, const struct xt_entry_match *match, int numeric)
 {
-	const struct xt_rpfilter_info *info = (const void *) match->data;
-
-	printf(" rpfilter");
-	rpfilter_show("", info->flags);
+	rpfilter_show("", match, numeric);
 }
 
-static void
-rpfilter_save(const void *ip, const struct xt_entry_match *match)
+static void rpfilter_save(const void *ip, const struct xt_entry_match *match)
 {
-	const struct xt_rpfilter_info *info = (const void *) match->data;
-
-	rpfilter_show("--", info->flags);
+	rpfilter_show("--", match, false);
 }
 
 static int rpfilter_xlate(struct xt_xlate *xl,
 			  const struct xt_xlate_mt_params *params)
 {
-	const struct xt_rpfilter_info *info = (void *)params->match->data;
+	const struct xt_rpfilter_mtinfo1 *info = (void *)params->match->data;
 	bool invert = info->flags & XT_RPFILTER_INVERT;
 
 	if (info->flags & XT_RPFILTER_ACCEPT_LOCAL) {
@@ -132,21 +210,43 @@ static int rpfilter_xlate(struct xt_xlate *xl,
 	return 1;
 }
 
-static struct xtables_match rpfilter_match = {
-	.family		= NFPROTO_UNSPEC,
-	.name		= "rpfilter",
-	.version	= XTABLES_VERSION,
-	.size		= XT_ALIGN(sizeof(struct xt_rpfilter_info)),
-	.userspacesize	= XT_ALIGN(sizeof(struct xt_rpfilter_info)),
-	.help		= rpfilter_help,
-	.print		= rpfilter_print,
-	.save		= rpfilter_save,
-	.x6_parse	= rpfilter_parse,
-	.x6_options	= rpfilter_opts,
-	.xlate		= rpfilter_xlate,
+static struct xtables_match rpfilter_mt_reg[] = {
+	{
+		.family		= NFPROTO_UNSPEC,
+		.name		= "rpfilter",
+		.version	= XTABLES_VERSION,
+		.revision	= 0,
+		.size		= XT_ALIGN(sizeof(struct xt_rpfilter_mtinfo0)),
+		.userspacesize	= XT_ALIGN(sizeof(struct xt_rpfilter_mtinfo0)),
+		.help		= rpfilter_help,
+		.print		= rpfilter_print,
+		.save		= rpfilter_save,
+		.x6_parse	= rpfilter_parse,
+		.x6_options	= rpfilter_opts,
+		.xlate		= rpfilter_xlate,
+	},
+	{
+		.family		= NFPROTO_UNSPEC,
+		.name		= "rpfilter",
+		.version	= XTABLES_VERSION,
+		.revision	= 1,
+		.size		= XT_ALIGN(sizeof(struct xt_rpfilter_mtinfo1)),
+		.userspacesize	= XT_ALIGN(sizeof(struct xt_rpfilter_mtinfo1)),
+		.help		= rpfilter_help,
+		.print		= rpfilter_print,
+		.save		= rpfilter_save,
+		.x6_parse	= rpfilter_parse,
+		.x6_options	= rpfilter_opts,
+		.xlate		= rpfilter_xlate,
+	},
 };
 
 void _init(void)
 {
-	xtables_register_match(&rpfilter_match);
+	devgroups = xtables_lmap_init(f_devgroups);
+	if (devgroups == NULL && errno != ENOENT)
+		fprintf(stderr, "Warning: %s: %s\n", f_devgroups,
+			strerror(errno));
+
+	xtables_register_matches(rpfilter_mt_reg, ARRAY_SIZE(rpfilter_mt_reg));
 }
