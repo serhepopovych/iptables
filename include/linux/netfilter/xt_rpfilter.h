793197365c1dcebba75e2fe5b356f47a2928f565
Serhey Popovych <serhe.popovych@gmail.com>
2017-11-06 19:51:20 +0200

    libxt_rpfilter: add revision 1 support
    Implement userspace part for revision 1 of ipt_rpfilter and
    ip6t_rpfilter which introduces support of prefixlen and devgroup
    match options.
    
    Make examples in man page more readable.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- include/linux/netfilter/xt_rpfilter.h
+++ include/linux/netfilter/xt_rpfilter.h
@@ -4,14 +4,25 @@
 #include <linux/types.h>
 
 enum {
-	XT_RPFILTER_LOOSE = 1 << 0,
-	XT_RPFILTER_VALID_MARK = 1 << 1,
-	XT_RPFILTER_ACCEPT_LOCAL = 1 << 2,
-	XT_RPFILTER_INVERT = 1 << 3,
+	XT_RPFILTER_LOOSE		= 1 << 0,
+	XT_RPFILTER_VALID_MARK		= 1 << 1,
+	XT_RPFILTER_ACCEPT_LOCAL	= 1 << 2,
+	XT_RPFILTER_INVERT		= 1 << 3,
+	XT_RPFILTER_PREFIXLEN		= 1 << 4,
+	XT_RPFILTER_PREFIXLEN_INVERT	= 1 << 5,
+	XT_RPFILTER_GROUP		= 1 << 6,
+	XT_RPFILTER_GROUP_INVERT	= 1 << 7,
 };
 
-struct xt_rpfilter_info {
+struct xt_rpfilter_mtinfo0 {
 	__u8 flags;
+};
+
+struct xt_rpfilter_mtinfo1 {
+	__u8 flags;		/* revision 0 compat */
+	__u8 prefixlen;
+	__u32 group;
+	__u32 group_mask;
 };
 
 #endif
